f1bcc8cfeb2bcee98c7d47db330504526f7ad9f2
KPOTOH <axepon@mail.ru>
2021-06-13 02:25:38 +0200

    DAG1 done

---
+++ airflow_ml_dags/dags/DAG2_train_model.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.utils.dates import days_ago
+
+DEFAULT_VOLUME = '/home/mr/MADE/DS-22/ml_in_prod/airflow_ml_dags/data:/data'
+
+default_args = {
+    "owner": "airflow",
+    "email": ["airflow@example.com"],
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "DAG1_train_model",
+    default_args=default_args,
+    schedule_interval="@weekly",
+    start_date=days_ago(3)
+) as dag:
+    start_task = DummyOperator(task_id='start-generation')
+    generate_data = DockerOperator(
+        task_id="docker-airflow-generate",
+        image="airflow-generate",
+        command="/data/raw/{{ ds }}",
+        network_mode="bridge",
+        do_xcom_push=False,
+        volumes=[DEFAULT_VOLUME]
+    )
+
+    end_task = DummyOperator(task_id='end-generation')
+
+    start_task >> generate_data >> end_task
